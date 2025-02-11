fb216f3d3b212c6bd1da5bc34caa4174945702a8
gulu43 <145833927+gulu43@users.noreply.github.com>
2025-02-11 11:11:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 // Complete the js code
 function Car(make, model) {
-	this.make = make;
-	this.model = model;
-}
-Car.prototype.getMakeModel = function(){
-    return `${this._make} ${this._model}`;
+    this.make = make;
+    this.model = model;
 }
 
+Car.prototype.getMakeModel = function() {
+    return `${this.make} ${this.model}`;
+};
 
 function SportsCar(make, model, topSpeed) {
-	Car.call(this,make,model);
-	this.make = make;
-	this.model = model;
-	this.topSpeed = topSpeed;
+    Car.call(this, make, model); // Call Car constructor
+    this.topSpeed = topSpeed;
 }
-SportsCar.prototype.getTopSpeed = function(){
-    return this._topSpeed ;
-}
+
+// Set up inheritance *before* defining methods
 SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+SportsCar.prototype.getTopSpeed = function() {
+    return this.topSpeed;
+};
+
+
 
 // Do not change the code below
 window.Car = Car;
